503ce0b0d9f6fa29e7fed6ca39cd143d522f7597
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-10-16 20:20:48 +0200

    Added default messages Array for containing all messages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ import express from "express";
 import process from "process";
 
 const PORT = process.env.PORT;
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
 const app = express();
 
 app.get("/", (req, res) => {
