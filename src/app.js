bd432bbef9ebf065dc8c1dc4a87faaa504048628
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-10-17 16:58:02 +0200

    Added updating of messages from POST /new and redirect to /

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ const messages = [
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.set({ "Content-Type": "text/html" });
@@ -34,6 +35,14 @@ app.get("/new", (req, res) => {
     res.set({ "Content-Type": "text/html" });
     res.status(200);
     res.render("form");
+});
+
+app.post("/new", (req, res) => {
+    console.log(req.body);
+
+    messages.push({ text: req.body.messageContent, user: req.body.userName, added: new Date() });
+
+    res.redirect("/");
 });
 
 app.listen(PORT);
