2305a40b19a790849d0f8653a186cc242693fef5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-10-18 18:25:44 +0200

    Created getOriginalMessage to get the original messages content and made it check the length of these two arrays

--- src/scripts/updateList.js
+++ src/scripts/updateList.js
@@ -1,13 +1,29 @@
+let messages;
+
+async function getOriginalMessage() {
+    let originalResponse = await fetch("/messages");
+    let originalMessages = await originalResponse.json();
+
+    messages = originalMessages;
+}
+
+getOriginalMessage();
+
 setInterval(async () => {
     let updatedResponse = await fetch("/messages");
 
     let updatedMessages = await updatedResponse.json();
 
     console.log(updatedResponse.status, updatedMessages);
+    console.log(messages, updatedMessages);
 
-    if (updatedResponse.status === 200) {
+    if (updatedResponse.status === 200 && messages.length < updatedMessages.length) {
         for (let i = 0; i < updatedMessages.length; i++) {
-            
+            console.log(true);
         }
+
+        messages = updatedMessages;
+    } else if (updatedResponse.status === 200 && messages.length > updatedMessages.length) {
+        location.reload();
     }
 }, 4000);
