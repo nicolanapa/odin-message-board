5a4f9471eb8162b6bffe0dcf038952d00f93b686
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-10-19 12:58:11 +0200

    Added a checking if a user tries to query an ID outside of messages.length

--- src/app.js
+++ src/app.js
@@ -43,6 +43,13 @@ app.get("/messages", (req, res) => {
 
 app.get("/messages/:id", (req, res) => {
     res.set({ "Content-Type": "text/html" });
+
+    if (messages.length < req.params.id) {
+        res.status(404);
+        res.send("<h1>Message of ID " + req.params.id + " doesn't exist!</h1>");
+        throw new Error("Message of ID " + req.params.id + " doesn't exist!");
+    }
+
     res.status(200);
     res.render("viewMessage", { title: "Mini Messageboard", message: messages[req.params.id], id: req.params.id });
 });
