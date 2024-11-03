141ca4866cf94fe77a939253683b397201ea0293
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-03 14:18:47 +0100

    Changed the Routers to use the db queries instead of the messages Array

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import * as db from "../db/queries.js";
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => {
+indexRouter.get("/", async (req, res) => {
     res.set({ "Content-Type": "text/html" });
     res.status(200);
-    res.render("index", { title: "Mini Messageboard", messages: messages });
+    res.render("index", { title: "Mini Messageboard", messages: await db.getAllMessages() });
 });
 
 export { indexRouter };
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,23 +1,26 @@
 import { Router } from "express";
+import * as db from "../db/queries.js";
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", (req, res) => {
+messagesRouter.get("/", async (req, res) => {
     res.status(200);
-    res.json(messages);
+    res.json(await db.getAllMessages());
 });
 
-messagesRouter.get("/:id", (req, res) => {
+messagesRouter.get("/:id", async (req, res) => {
+    const message = await db.getMessage(req.params.id);
+
     res.set({ "Content-Type": "text/html" });
 
-    if (messages.length < req.params.id || req.params.id < 0 || !Number.isInteger(Number(req.params.id))) {
+    if (db.getCountMessages() < req.params.id || req.params.id < 0 || !Number.isInteger(Number(req.params.id))) {
         res.status(404);
         res.send("<h1>Message of ID " + req.params.id + " doesn't exist!</h1>");
         throw new Error("Message of ID " + req.params.id + " doesn't exist!");
     }
 
     res.status(200);
-    res.render("viewMessage", { title: "Mini Messageboard", message: messages[req.params.id], id: req.params.id });
+    res.render("viewMessage", { title: "Mini Messageboard", message: message, id: req.params.id });
 });
 
 export { messagesRouter };
