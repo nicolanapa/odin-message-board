2470bebc60ff5d71b4255b971310a9d949cbd77b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-03 13:07:46 +0100

    Added Client const to Query the SQL const

--- src/db/populateDb.js
+++ src/db/populateDb.js
@@ -2,7 +2,7 @@ import process from "process";
 import pg from "pg";
 import returnDate from "../scripts/returnDate.js";
 
-const client = pg.Client;
+const Client = pg.Client;
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS messages (
@@ -16,3 +16,13 @@ INSERT INTO messages (text, user, added)
 VALUES ("Hi there!", "Amando", "${returnDate()}"),
     ("Hello World!", "Charles", "${returnDate()}");
 `;
+
+async () => {
+    const client = new Client({ connectionString: process.env.DB_CONNECTION_STRING });
+
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+
+    console.log("Created messages TABLE with DEFAULT VALUES");
+};
