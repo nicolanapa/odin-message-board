eab62cecb5699b0c864657eb4c365d0e50fd7995
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-03 15:15:22 +0100

    Fixed Query getting stuck by adding a ternary operator to check on if :id is a Number

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -9,16 +9,12 @@ messagesRouter.get("/", async (req, res) => {
 });
 
 messagesRouter.get("/:id", async (req, res) => {
-    const message = await db.getMessage(req.params.id);
-
+    const message = !Number.isInteger(Number(req.params.id))
+        ? undefined
+        : await db.getMessage(req.params.id);
     res.set({ "Content-Type": "text/html" });
 
-    if (
-        db.getCountMessages() < req.params.id ||
-        req.params.id < 0 ||
-        !Number.isInteger(Number(req.params.id)) ||
-        message === undefined
-    ) {
+    if (db.getCountMessages() < req.params.id || req.params.id <= 0 || message === undefined) {
         res.status(404);
         res.send("<h1>Message of ID " + req.params.id + " doesn't exist!</h1>");
         throw new Error("Message of ID " + req.params.id + " doesn't exist!");
