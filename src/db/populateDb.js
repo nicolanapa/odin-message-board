25bf18551faf5162b5ec6c50637a25b8ac111645
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-11-03 13:25:05 +0100

    Fixed the SQL Query to use single quotes and changed user to userName for keyword reasons

--- src/db/populateDb.js
+++ src/db/populateDb.js
@@ -8,13 +8,13 @@ const SQL = `
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     text VARCHAR (250),
-    user VARCHAR (32),
+    userName VARCHAR (32),
     added CHAR (20)
 );
 
-INSERT INTO messages (text, user, added) 
-VALUES ("Hi there!", "Amando", "${returnDate()}"),
-    ("Hello World!", "Charles", "${returnDate()}");
+INSERT INTO messages (text, userName, added) 
+VALUES ('Hi there!', 'Amando', '${returnDate()}'),
+    ('Hello World!', 'Charles', '${returnDate()}');
 `;
 
 async function main() {
